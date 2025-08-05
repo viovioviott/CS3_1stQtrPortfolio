b54732cb503993476107f4e4ac5f002d7aca5a51
github username <your email used in github>
2025-08-05 08:48:42 +0800

    ng commit

--- public/script.js
+++ public/script.js
@@ -1,1 +1,7 @@
 //no script tag needed here?
+
+var age = 14;
+var maxAge = 60;
+var numPerDay = 3;
+var totalRequired = (numPerDay * 365) * (maxAge - age) 
+    document.write("You will need " + totalRequired + " cookies to last you until the ripe old age of " + maxAge)
