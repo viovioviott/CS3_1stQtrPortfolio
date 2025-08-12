ff9a75be8b3fb0c5bd6ace68750164ff81d42753
github username <your email used in github>
2025-08-12 08:51:37 +0800

    silly commit

--- public/script.js
+++ public/script.js
@@ -1,8 +1,8 @@
 //no script tag needed here?
 
-var age = window.prompt("What is your current age? : ", "14");
-var maxAge = window.prompt("What do you think would be your maximum age? : ", "60");
-var numPerDay = window.prompt("Now, how many cookies per day do you think you could eat? : ", "3");
+//var age = window.prompt("What is your current age? : ", "14");
+//var maxAge = window.prompt("What do you think would be your maximum age? : ", "60");
+//var numPerDay = window.prompt("Now, how many cookies per day do you think you could eat? : ", "3");
 let sure = confirm("Are your values " + age + ", " + maxAge + ", and " + numPerDay + " correct?")
 
 if (sure == true) {
