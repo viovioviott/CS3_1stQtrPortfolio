d9f12eb61fb50d2a2516731c311bb2b11d6ae5b0
github username <your email used in github>
2025-08-12 08:20:33 +0800

    fifth commit

--- public/script.js
+++ public/script.js
@@ -1,7 +1,15 @@
 //no script tag needed here?
 
-var age = 14;
-var maxAge = 60;
-var numPerDay = 3;
-var totalRequired = (numPerDay * 365) * (maxAge - age) 
+var age = window.prompt("What is your current age? : ", "14");
+var maxAge = window.prompt("What do you think would be your maximum age? : ", "60");
+var numPerDay = window.prompt("Now, how many cookies per day do you think you could eat? : ", "3");
+let sure = confirm("Are your values " + age + ", " + maxAge + ", and " + numPerDay + " correct?")
+
+if (sure == true) {
+    var totalRequired = (numPerDay * 365) * (maxAge - age) 
     document.write("You will need " + totalRequired + " cookies to last you until the ripe old age of " + maxAge)
+}
+
+else {
+    alert("Run your code")
+}
